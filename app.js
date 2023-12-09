0c4c5e3a39c891dc1a3c3d2d3f207881c54b31c4
shakyab <shakya.b@northeastern.edu>
2023-12-09 12:18:25 -0500

    Updated

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 import "dotenv/config";
 import session from "express-session";
 import express from 'express';
-
-
-import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
 import Hello from "./hello.js"
+import cors from "cors";
+
+const CONNECTION_STRING =
+  process.env.REACT_APP_DB_CONNECTION_STRING ||
+  "mongodb://127.0.0.1:27017/kanbas";
+
+  mongoose.connect(CONNECTION_STRING);
+
 const app = express()
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    //origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
+  }
+
+));
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  
+  app.use(session(sessionOptions));
+
 app.use(express.json());
+
+UserRoutes(app);
 Hello(app)
+
 app.listen(4000)
+//app.listen(process.env.PORT || 4000);
